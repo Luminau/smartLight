4fb951de097a78a377d70d58545b0769dc5bdbc9
Luminau <77398134+Luminau@users.noreply.github.com>
2022-03-27 23:24:30 +0800

    完成手动设置日期

--- app/src/main/java/com/light1/ui/time_management/TimeManager.java
+++ app/src/main/java/com/light1/ui/time_management/TimeManager.java
@@ -1,5 +1,6 @@
 package com.light1.ui.time_management;
 
+import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -8,6 +9,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.widget.DatePicker;
 import android.widget.TimePicker;
 
 import com.light1.databinding.FragmentTimeManagementBinding;
@@ -16,8 +18,11 @@ import java.util.Locale;
 
 public class TimeManager extends Fragment {
 
-    private Button mtimeButton;
+    private Button mTimeButton;
+    private Button mDateButton;
+
     protected int hour, minute;
+    protected int year, month, date;
 
     private FragmentTimeManagementBinding binding;
 
@@ -43,11 +48,18 @@ public class TimeManager extends Fragment {
     }
 
     public void initParameters(){
-        mtimeButton = binding.timeButton;
-        mtimeButton.setOnClickListener(new View.OnClickListener() {
+        mTimeButton = binding.timeButton;
+        mDateButton = binding.dateButton;
+        mTimeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 popTimePicker(view);
+            }
+        });
+        mDateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                popDatePicker(view);
             }
         });
     }
@@ -56,20 +68,34 @@ public class TimeManager extends Fragment {
 
     public void popTimePicker(View view)
     {
-        int style = TimePickerDialog.THEME_HOLO_LIGHT;
+//        int style = TimePickerDialog.THEME_HOLO_LIGHT;
 
         TimePickerDialog.OnTimeSetListener onTimeSetListener = new TimePickerDialog.OnTimeSetListener() {
             @Override
             public void onTimeSet(TimePicker timePicker, int selectHour, int selectMinute) {
                 hour = selectHour;
                 minute = selectMinute;
-                mtimeButton.setText(String.format(Locale.getDefault(),"%02d:%02d",hour,minute));
+                mTimeButton.setText(String.format(Locale.getDefault(),"%02d:%02d",hour,minute));
             }
         };
 
-        TimePickerDialog timePickerDialog = new TimePickerDialog(getContext(), style, onTimeSetListener, hour, minute, true);
+        TimePickerDialog timePickerDialog = new TimePickerDialog(getContext(), onTimeSetListener, hour, minute, true);
 
         timePickerDialog.setTitle("设置时间");
         timePickerDialog.show();
+    }
+
+    public void popDatePicker(View view)
+    {
+        DatePickerDialog.OnDateSetListener onDateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int selectYear, int selectMonth, int selectDate) {
+                year = selectYear;
+                month = selectMonth;
+                date = selectDate;
+            }
+        };
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(getContext(), onDateSetListener, year, month, date);
     }
 }
