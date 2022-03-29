76b04baf07277dad4200cabd995db3ce99e61e3c
Luminau <77398134+Luminau@users.noreply.github.com>
2022-03-29 13:20:36 +0800

    完成数据库基础搭建

--- app/src/main/java/com/light1/data/TaskDao.java
+++ app/src/main/java/com/light1/data/TaskDao.java
@@ -1,8 +1,10 @@
 package com.light1.data;
 
 import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
 import android.arch.persistence.room.Insert;
 import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Update;
 
 import androidx.lifecycle.LiveData;
 
@@ -24,4 +26,10 @@ public interface TaskDao {
 
     @Query("SELECT * FROM task_table WHERE task_table.task_id == :id")
     LiveData<Task> get(long id);
+
+    @Update
+    void update(Task task);
+
+    @Delete
+    void delete(Task task);
 }
--- app/src/main/java/com/light1/util/TaskRoomDatabase.java
+++ app/src/main/java/com/light1/util/TaskRoomDatabase.java
@@ -1,6 +1,8 @@
 package com.light1.util;
 
 import android.arch.persistence.db.SupportSQLiteDatabase;
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Database;
 import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
 import android.content.Context;
@@ -8,11 +10,13 @@ import android.content.Context;
 import androidx.annotation.NonNull;
 
 import com.light1.data.TaskDao;
+import com.light1.model.Task;
 
 import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+@Database(entities = {Task.class}, version = 1, exportSchema = false)
 public abstract class TaskRoomDatabase extends RoomDatabase {
     public static final int NUMBER_OF_THREADS = 4;
     public static final String DATASE_NAME = "todolister_database";
@@ -24,6 +28,8 @@ public abstract class TaskRoomDatabase extends RoomDatabase {
             super.onCreate(db);
             databaseWriterExcutor.execute(() -> {
                 // TODO: 2022/3/29 invoke dao and write
+                TaskDao taskDao = INSTANCE.taskDao();
+                taskDao.deleteAll(); //清楚数据库内所有已有数据
             });
         }
     };
