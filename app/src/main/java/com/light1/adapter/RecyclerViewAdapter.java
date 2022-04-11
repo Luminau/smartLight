920f85570779d2a81291534c13c816ce430f8f7e
Luminau <77398134+Luminau@users.noreply.github.com>
2022-04-11 10:32:25 +0800

    使用if语句替代switch语句尝试修复编辑任务时会删除原任务的问题

--- app/src/main/java/com/light1/adapter/RecyclerViewAdapter.java
+++ app/src/main/java/com/light1/adapter/RecyclerViewAdapter.java
@@ -69,12 +69,11 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
         public void onClick(View view) {
             Task currTask = taskList.get(getAdapterPosition());
             int id = view.getId();
-            switch (id){
-                case R.id.todo_row_layout:
-                    onTodoClickListener.onTodoClick(currTask);
-                case R.id.todo_radio_button:
-                    onTodoClickListener.onTodoRadioButtonClick(currTask);
-
+            if(id == R.id.todo_row_layout){
+                onTodoClickListener.onTodoClick(currTask);
+            }
+            else if(id == R.id.todo_radio_button){
+                onTodoClickListener.onTodoRadioButtonClick(currTask);
             }
 
         }
